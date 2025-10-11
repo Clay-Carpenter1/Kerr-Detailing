414afb7715f22145816a866a98bf8f281e174a70
Clay-Carpenter1 <candocarp@gmail.com>
2025-10-11 14:46:51 -0500

    vite config mod

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteImagemin } from 'vite-plugin-imagemin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteImagemin({
-      mozjpeg: {
-        quality: 80,
-      },
-      pngquant: {
-        quality: [0.8, 0.9],
-      },
-    })
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': '/src'
