16e172e3fe9ad267bc014004208c0f5ded6fcbfb
Clay Carpenter <clay.carpenter@powers-hvac.com>
2026-03-06 14:59:06 -0600

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { AuthProvider } from './contexts/AuthContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -31,6 +32,7 @@ function App() {
           </main>
           <Footer />
         </div>
+        <Analytics />
       </Router>
     </AuthProvider>
   );
